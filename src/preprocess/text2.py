1d741ca9762a7ac94ef21522fd9d3ec02cb57f2d
therealsharath <palathingal@gatech.edu>
2021-04-04 16:55:58 -0400

    Made deletion into a function

--- src/preprocess/text2.py
+++ src/preprocess/text2.py
@@ -3,11 +3,20 @@
 
 import pandas as pd
 
-data = pd.read_csv('../../data/nyt-articles-2020.csv')
-l1 = len(data)
-print(l1)
 
-for feature in data:
-    data = data[data[feature].notnull()]
 
-print(len(data))
+#main function
+def delete_incomplete_columns(file, to_be_deleted):
+    data = pd.read_csv('../../data/'+file)
+    for tbd in to_be_deleted:
+        del data[tbd]
+
+    for feature in data:
+        data = data[data[feature].notnull()]
+
+    data.to_csv('../../processed_data/complete-nyt-articles-2020.csv')
+
+#calling the function on articles
+file = 'nyt-articles-2020.csv' #param1 i.e. which file we want to clean
+to_be_deleted = ['newsdesk', 'material', 'keywords', 'pub_date', 'uniqueID'] #param2 columns we want to delete
+delete_incomplete_columns(file, to_be_deleted)
