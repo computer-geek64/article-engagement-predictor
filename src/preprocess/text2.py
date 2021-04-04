fcdbbac8580f2fd258177777336925c48765ab50
therealsharath <palathingal@gatech.edu>
2021-04-04 18:07:38 -0400

    Added stopword function

--- src/preprocess/text2.py
+++ src/preprocess/text2.py
@@ -2,21 +2,31 @@
 # text2.py
 
 import pandas as pd
-
-
+from stop_words import get_stop_words
 
 #main function
-def delete_incomplete_columns(file, to_be_deleted):
-    data = pd.read_csv('../../data/'+file)
+def delete_incomplete_columns(file, to_be_deleted=[], not_to_be_deleted=[]):
+    data = pd.read_csv('../../data/' + file)
+    features = data.keys()
+
     for tbd in to_be_deleted:
         del data[tbd]
-
-    for feature in data:
+    
+    for feature in features:
         data = data[data[feature].notnull()]
 
     data.to_csv('../../processed_data/complete-nyt-articles-2020.csv')
 
 #calling the function on articles
-file = 'nyt-articles-2020.csv' #param1 i.e. which file we want to clean
-to_be_deleted = ['newsdesk', 'material', 'keywords', 'pub_date', 'uniqueID'] #param2 columns we want to delete
-delete_incomplete_columns(file, to_be_deleted)
+tbd = ['newsdesk', 'keywords', 'pub_date'] #param2 columns we want to delete
+delete_incomplete_columns('nyt-articles-2020.csv', to_be_deleted=tbd)
+
+#calling the function on articles
+# tbd = ['newsdesk', 'material', 'keywords', 'pub_date'] #param2 columns we want to delete
+# delete_incomplete_columns('nyt-articles-2020.csv', to_be_deleted=tbd)
+
+
+#stopwords
+def is_stop_word(word):
+    stop_word = get_stop_words('en')
+    return word in stop_word
