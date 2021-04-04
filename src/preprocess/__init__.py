268264d41bfa28e23a47ef291cb6b25fc13dae9a
therealsharath <palathingal@gatech.edu>
2021-04-04 16:42:23 -0400

    Cleaned articles with missing data

--- src/preprocess/__init__.py
+++ src/preprocess/__init__.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
 # __init__.py
-
--- src/preprocess/text2.py
+++ src/preprocess/text2.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 # text2.py
 
+import pandas as pd
+
+data = pd.read_csv('../../data/nyt-articles-2020.csv')
+l1 = len(data)
+print(l1)
+
+for feature in data:
+    data = data[data[feature].notnull()]
+
+print(len(data))
