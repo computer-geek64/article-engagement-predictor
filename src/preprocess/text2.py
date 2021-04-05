bbabb447c6ff949d931fe094e73df5e921d4fd73
therealsharath <palathingal@gatech.edu>
2021-04-04 20:05:07 -0400

    Completed text2

--- src/preprocess/text2.py
+++ src/preprocess/text2.py
@@ -5,26 +5,32 @@ import pandas as pd
 from stop_words import get_stop_words
 
 #main function
-def delete_incomplete_columns(file, to_be_deleted=[], not_to_be_deleted=[]):
+def delete_incomplete_columns(file, to_be_deleted=[], not_to_be_deleted=[], special=False):
     data = pd.read_csv('../../data/' + file)
-    features = data.keys()
 
     for tbd in to_be_deleted:
-        del data[tbd]
-    
-    for feature in features:
-        data = data[data[feature].notnull()]
+        del data[tbd]   
 
-    data.to_csv('../../processed_data/complete-nyt-articles-2020.csv')
+    if len(not_to_be_deleted) > 0:
+        data = data.filter(not_to_be_deleted)
+    
+    for feature in data:
+        data = data[data[feature].notnull()]
+    
+    if special:
+        unwanted = ['Interactive Feature', 'Obituary (Obit)', 'briefing', 'Letter']
+        data = data[data['material'].isin(unwanted)]
+    
+    data.to_csv('../../processed_data/complete-'+file)
+
 
 #calling the function on articles
 tbd = ['newsdesk', 'keywords', 'pub_date'] #param2 columns we want to delete
-delete_incomplete_columns('nyt-articles-2020.csv', to_be_deleted=tbd)
+delete_incomplete_columns('nyt-articles-2020.csv', to_be_deleted=tbd, special=True)
 
 #calling the function on articles
-# tbd = ['newsdesk', 'material', 'keywords', 'pub_date'] #param2 columns we want to delete
-# delete_incomplete_columns('nyt-articles-2020.csv', to_be_deleted=tbd)
-
+tb = ['commentBody', 'articleID'] #param2 columns we want to delete
+delete_incomplete_columns('nyt-comments-2020.csv', not_to_be_deleted=tb)
 
 #stopwords
 def is_stop_word(word):
