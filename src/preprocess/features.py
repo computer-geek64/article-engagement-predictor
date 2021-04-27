c24814387f94fd076c0411b643c22f01e60d503d
Ashish D'Souza <adsouza@gatech.edu>
2021-04-27 17:08:53 -0400

    Fixed article row drops

--- src/preprocess/features.py
+++ src/preprocess/features.py
@@ -8,10 +8,6 @@ import pandas as pd
 def delete_incomplete_columns(file, to_be_deleted=[], not_to_be_deleted=[], special=False):
     data = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'data', file))
 
-    if special:
-        unwanted = ['Interactive Feature', 'Obituary (Obit)', 'briefing', 'Letter']
-        data = data[~data['section'].isin(unwanted)]
-
     for tbd in to_be_deleted:
         del data[tbd]
 
@@ -20,6 +16,10 @@ def delete_incomplete_columns(file, to_be_deleted=[], not_to_be_deleted=[], spec
 
     for feature in data:
         data = data[data[feature].notnull()]
+
+    if special:
+        unwanted = ['Interactive Feature', 'Obituary (Obit)', 'briefing', 'Letter']
+        data = data[~data['material'].isin(unwanted)]
 
     data.to_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'data', os.path.splitext(file)[0] + '-dropped.csv'), index=False)
 
@@ -62,7 +62,7 @@ def drop_features():
     delete_incomplete_columns('nyt-articles-2020.csv', to_be_deleted=tbd, special=True)
 
     # Run one-hot encoding on articles
-    one_hot_encoding('nyt-articles-2020-dropped.csv')
+    # one_hot_encoding('nyt-articles-2020-dropped.csv')
 
     # calling the function on comments
     tbd = ['commentBody', 'articleID']  # param2 columns we want to delete
